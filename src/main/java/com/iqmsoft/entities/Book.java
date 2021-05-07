c9aee030646aa57950ab87d54f38d358a832a005
Muruga <davanon2014@gmail.com>
2021-05-07 11:04:17 +0530

    Up

--- src/main/java/com/iqmsoft/entities/Book.java
+++ src/main/java/com/iqmsoft/entities/Book.java
@@ -3,9 +3,11 @@ package com.iqmsoft.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.NotBlank;
+
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 import java.util.Date;
 
 @AllArgsConstructor @NoArgsConstructor @Data
